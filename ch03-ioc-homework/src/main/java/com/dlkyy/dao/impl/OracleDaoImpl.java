317e963a932f7086ddfe51ca1c1e7dc393fc51d8
dddllk <474464152@qq.com>
2021-04-20 20:18:25 +0800

    注解完成作业

--- ch03-ioc-homework/src/main/java/com/dlkyy/dao/impl/OracleDaoImpl.java
+++ ch03-ioc-homework/src/main/java/com/dlkyy/dao/impl/OracleDaoImpl.java
@@ -2,10 +2,12 @@ package com.dlkyy.dao.impl;
 
 import com.dlkyy.dao.UserDao;
 import com.dlkyy.domain.SysUser;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by dlkyy on 2021/4/19 20:00
  */
+@Repository("oracleDao")
 public class OracleDaoImpl implements UserDao {
   @Override
   public void insertUser(SysUser user) {
