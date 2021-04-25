977022634f345509b26e00c24985676b8d0efdcb
dlkyy <474464152@qq.com>
2021-04-25 21:22:51 +0800

    前置通知

---
+++ ch06-aop-aspect/src/main/java/com/dlkyy/ba01/SomeService.java
@@ -0,0 +1,8 @@
+package com.dlkyy.ba01;
+
+/**
+ * Created by dlkyy on 2021/4/25 19:58
+ */
+public interface SomeService {
+  void doSome(String name, Integer age);
+}
---
+++ ch06-aop-aspect/src/test/java/com/dlkyy/AppTest.java
@@ -0,0 +1,24 @@
+package com.dlkyy;
+
+import com.dlkyy.ba01.SomeService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void test(){
+        String config = "applicationContext.xml";
+        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+        // 从容器中获取目标对象
+        SomeService proxy = (SomeService) ac.getBean("someService");
+        System.out.println("proxy= " + proxy.getClass().getName());
+        // 通过代理的对象执行方法，实现目标方法执行时，增强了功能
+        proxy.doSome("jq", 16);
+
+    }
+
+}
